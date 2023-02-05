7f064ba22c727601b1f9b73618d18c54239baf0b
Akshaygowda-M <akshaygowda370@gmail.com>
2023-02-05 14:34:51 +0530

    server laod monitoring

---
+++ server_load.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check server load
+load=$(uptime | awk -F'load average:' '{ print $2 }')
+
+# Check if load is high
+if echo "$load" | awk '{ exit $1>=2.0 }'; then
+  echo "High server load: $load"
+else
+  echo "Server load is normal: $load"
+fi
+
+date
